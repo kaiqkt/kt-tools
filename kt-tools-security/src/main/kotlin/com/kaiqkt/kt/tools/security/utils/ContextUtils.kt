0538ae5fe5677c3cee2c7deeb2f70ddacfe1d2fe
kaiqkt <kaiquebeserra1313@gmail.com>
2025-03-13 17:56:30 -0300

    added throw a exception when not found a value in security context

--- kt-tools-security/src/main/kotlin/com/kaiqkt/kt/tools/security/utils/ContextUtils.kt
+++ kt-tools-security/src/main/kotlin/com/kaiqkt/kt/tools/security/utils/ContextUtils.kt
@@ -1,17 +1,20 @@
 package com.kaiqkt.kt.tools.security.utils
 
 import com.kaiqkt.kt.tools.security.dtos.Authentication
+import com.kaiqkt.kt.tools.security.enums.ErrorType
+import com.kaiqkt.kt.tools.security.exception.AuthException
+import org.springframework.http.HttpStatus
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
 
 object ContextUtils {
-    fun <T> getValue(key: String, type: Class<T>): T? {
+    fun <T> getValue(key: String, type: Class<T>): T {
         val context: SecurityContext = SecurityContextHolder.getContext()
         val authentication: Authentication = context.authentication as Authentication
         val value = (authentication.details as Map<*, *>)[key]
 
         if (value == null) {
-            return null
+            throw AuthException("Invalid token", ErrorType.INVALID_TOKEN, HttpStatus.UNAUTHORIZED)
         }
 
         return type.cast(value)
